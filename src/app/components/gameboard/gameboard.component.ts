2cc91a5a750324507a5e0f7a15e7a6094855072a
clayton-scherer <cscherer93@aol.com>
2022-07-17 01:10:08 -0400

    Added upgrade buttons with price boundaries, intervals that increase the total count based on upgrades purchased.

--- src/app/components/gameboard/gameboard.component.ts
+++ src/app/components/gameboard/gameboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-gameboard',
@@ -7,14 +7,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameboardComponent implements OnInit {
   count: number = 0;
+  digitPrice: number = 10;
+  extraDigits: number = 0;
+  extraDigitMultiplier: number = 1;
+  handPrice: number = 25;
+  extraHands: number = 0;
+  extraHandMultiplier: number = 5;
+  feetPrice: number = 75;
+  extraFeet: number = 0;
+  extraFeetMultiplier: number = 9;
+  intervals: any  = {
+    extraDigitIntervals: undefined,
+    extraHandIntervals: undefined,
+    extraFeetIntervals: undefined,
+  };
   constructor() { }
 
   ngOnInit(): void {
     
   }
 
+  ngOnDestroy() {
+    clearInterval(this.intervals['extraDigitIntervals']);
+    clearInterval(this.intervals['extraHandIntervals']);
+    clearInterval(this.intervals['extraFeetIntervals']);
+  }
+
   incremenetCount() {
-    this.count++
+    this.count += 1;
+  }
+
+  purchaseExtraDigit() {
+    // Remove the current interval and Value Per Second.
+    // Add a new interval with the updated VPS.
+    clearInterval(this.intervals.extraDigitIntervals);
+
+     // Remove the cost from the total count;
+     this.count -= this.digitPrice;
+
+    const digitInterval = setInterval(() => {
+      this.count += (this.extraDigitMultiplier * this.extraDigits);
+    }, 1000);
+
+    Object.assign(this.intervals, {extraDigitIntervals: digitInterval});
+
+    this.extraDigits += 1;
+    this.digitPrice += this.extraDigits ** 1;
+  }
+
+  purchaseExtraHand() {
+    // Remove the current interval and Value Per Second.
+    // Add a new interval with the updated VPS.
+    clearInterval(this.intervals.extraHandIntervals);
+
+     // Remove the cost from the total count;
+     this.count -= this.handPrice;
+
+    const handInterval = setInterval(() => {
+      this.count += (this.extraHandMultiplier * this.extraHands);
+    }, 1000);
+
+    Object.assign(this.intervals, {extraHandIntervals: handInterval});
+
+    this.extraHands += 1;
+    this.handPrice += this.extraHands ** 2;
+  }
+
+  purchaseExtraFeet() {
+    // Remove the current interval and Value Per Second.
+    // Add a new interval with the updated VPS.
+    clearInterval(this.intervals.extraFeetIntervals);
+
+    // Remove the cost from the total count;
+    this.count -= this.feetPrice;
+
+    const feetInterval = setInterval(() => {
+      this.count += (this.extraFeetMultiplier * this.extraFeet);
+    }, 1000);
+
+    Object.assign(this.intervals, {extraFeetIntervals: feetInterval});
+
+    this.extraFeet += 1;
+    this.feetPrice += this.extraFeet ** 3;
   }
 
 }
