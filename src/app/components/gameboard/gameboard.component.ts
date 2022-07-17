f8003bf66d0af9301a3ba6a04f21ef82d830ba98
clayton-scherer <cscherer93@aol.com>
2022-07-17 01:24:06 -0400

    format document and move the purchase assignment above the setIntervals.

--- src/app/components/gameboard/gameboard.component.ts
+++ src/app/components/gameboard/gameboard.component.ts
@@ -3,29 +3,32 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
-  styleUrls: ['./gameboard.component.css']
+  styleUrls: ['./gameboard.component.css'],
 })
 export class GameboardComponent implements OnInit {
   count: number = 0;
+
   digitPrice: number = 10;
   extraDigits: number = 0;
   extraDigitMultiplier: number = 1;
+
   handPrice: number = 25;
   extraHands: number = 0;
   extraHandMultiplier: number = 5;
+
   feetPrice: number = 75;
   extraFeet: number = 0;
   extraFeetMultiplier: number = 9;
-  intervals: any  = {
+
+  intervals: any = {
     extraDigitIntervals: undefined,
     extraHandIntervals: undefined,
     extraFeetIntervals: undefined,
   };
-  constructor() { }
 
-  ngOnInit(): void {
-    
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   ngOnDestroy() {
     clearInterval(this.intervals['extraDigitIntervals']);
@@ -42,16 +45,17 @@ export class GameboardComponent implements OnInit {
     // Add a new interval with the updated VPS.
     clearInterval(this.intervals.extraDigitIntervals);
 
-     // Remove the cost from the total count;
-     this.count -= this.digitPrice;
-
-    const digitInterval = setInterval(() => {
-      this.count += (this.extraDigitMultiplier * this.extraDigits);
-    }, 1000);
-
-    Object.assign(this.intervals, {extraDigitIntervals: digitInterval});
+    // Remove the cost from the total count;
+    this.count -= this.digitPrice;
 
     this.extraDigits += 1;
+
+    const digitInterval = setInterval(() => {
+      this.count += this.extraDigitMultiplier * this.extraDigits;
+    }, 1000);
+
+    Object.assign(this.intervals, { extraDigitIntervals: digitInterval });
+
     this.digitPrice += this.extraDigits ** 1;
   }
 
@@ -60,16 +64,17 @@ export class GameboardComponent implements OnInit {
     // Add a new interval with the updated VPS.
     clearInterval(this.intervals.extraHandIntervals);
 
-     // Remove the cost from the total count;
-     this.count -= this.handPrice;
-
-    const handInterval = setInterval(() => {
-      this.count += (this.extraHandMultiplier * this.extraHands);
-    }, 1000);
-
-    Object.assign(this.intervals, {extraHandIntervals: handInterval});
+    // Remove the cost from the total count;
+    this.count -= this.handPrice;
 
     this.extraHands += 1;
+
+    const handInterval = setInterval(() => {
+      this.count += this.extraHandMultiplier * this.extraHands;
+    }, 1000);
+
+    Object.assign(this.intervals, { extraHandIntervals: handInterval });
+
     this.handPrice += this.extraHands ** 2;
   }
 
@@ -81,14 +86,14 @@ export class GameboardComponent implements OnInit {
     // Remove the cost from the total count;
     this.count -= this.feetPrice;
 
+    this.extraFeet += 1;
+
     const feetInterval = setInterval(() => {
-      this.count += (this.extraFeetMultiplier * this.extraFeet);
+      this.count += this.extraFeetMultiplier * this.extraFeet;
     }, 1000);
 
-    Object.assign(this.intervals, {extraFeetIntervals: feetInterval});
+    Object.assign(this.intervals, { extraFeetIntervals: feetInterval });
 
-    this.extraFeet += 1;
     this.feetPrice += this.extraFeet ** 3;
   }
-
 }
